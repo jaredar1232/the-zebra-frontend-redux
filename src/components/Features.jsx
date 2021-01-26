49a68c44a09256fab7624b779010b8a8f041eacc
Jared Rothenberg <jaredar@gmail.com>
2021-01-26 09:56:59 -0800

    refactor features component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -21,148 +21,41 @@ import { ReactComponent as IconFeaturesCreditCard } from "../assets/IconFeatures
 import { ReactComponent as IconFeatures247Support } from "../assets/IconFeatures247Support.svg";
 
 const Features = (props) => {
-  // Takes in a features array. Returns an array of feature elements
+  // Takes in a features array. Returns an array of feature components
   const buildFeatures = (featureNamesArray) => {
-    let featuresArray = [];
+    let featuresComponentArray = [];
 
-    // TODO: REFACTOR
-    /** 
-    I know... This "seems" hideous. Its just a for loop
-    looping over an array of feature names. Inside is a 
-    20 statement ternary. If a name matches a ternary then 
-    the svg component is pushed into the features array. 
-    Will refactor if time allows. Maybe a string to JSX library?
-    */
     for (let i = 0; i < featureNamesArray.length; i++) {
-      //   console.log(featureNamesArray[i].icon);
+      const featuresObject = {
+        IconFeaturesBuyOnline: <IconFeaturesBuyOnline key={i} />,
+        IconFeaturesLocalAgent: <IconFeaturesLocalAgent key={i} />,
+        IconFeaturesMobile: <IconFeaturesMobile key={i} />,
+        IconFeaturesPaperless: <IconFeaturesPaperless key={i} />,
+        IconFeaturesNewCarReplacement: (
+          <IconFeaturesNewCarReplacement key={i} />
+        ),
+        IconFeaturesImmediateCoverage: (
+          <IconFeaturesImmediateCoverage key={i} />
+        ),
+        IconFeaturesAutomaticPayment: <IconFeaturesAutomaticPayment key={i} />,
+        IconFeaturesOnlineIdCards: <IconFeaturesOnlineIdCards key={i} />,
+        IconFeaturesSR22: <IconFeaturesSR22 key={i} />,
+        IconFeaturesRoadside: <IconFeaturesRoadside key={i} />,
+        IconFeaturesRideshare: <IconFeaturesRideshare key={i} />,
+        IconFeaturesRentersBundle: <IconFeaturesRentersBundle key={i} />,
+        IconFeaturesRateLock: <IconFeaturesRateLock key={i} />,
+        IconFeaturesOnlineServicing: <IconFeaturesOnlineServicing key={i} />,
+        IconFeaturesNonOwner: <IconFeaturesNonOwner key={i} />,
+        IconFeaturesNoCreditCheck: <IconFeaturesNoCreditCheck key={i} />,
+        IconFeaturesLowDownPayment: <IconFeaturesLowDownPayment key={i} />,
+        IconFeaturesDriverExclusion: <IconFeaturesDriverExclusion key={i} />,
+        IconFeaturesCreditCard: <IconFeaturesCreditCard key={i} />,
+        IconFeatures247Support: <IconFeatures247Support key={i} />,
+      };
       let iconName = featureNamesArray[i].icon;
-
-      //   let element = `<${iconName} />`;
-      // featuresArray.push(JSON.parse(`<${iconName} />`));/
-
-      //   let pushable =
-      //     iconName === "IconFeaturesBuyOnline" ? (
-      //       <IconFeaturesBuyOnline />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesLocalAgent" ? (
-      //       <IconFeaturesLocalAgent />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesMobile" ? (
-      //       <IconFeaturesMobile />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesPaperless" ? (
-      //       <IconFeaturesPaperless />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesNewCarReplacement" ? (
-      //       <IconFeaturesNewCarReplacement />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesImmediateCoverage" ? (
-      //       <IconFeaturesImmediateCoverage />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesAutomaticPayment" ? (
-      //       <IconFeaturesAutomaticPayment />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesOnlineIdCards" ? (
-      //       <IconFeaturesOnlineIdCards />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesSR22" ? (
-      //       <IconFeaturesSR22 />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesRoadside" ? (
-      //       <IconFeaturesRoadside />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesRideshare" ? (
-      //       <IconFeaturesRideshare />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesRentersBundle" ? (
-      //       <IconFeaturesRentersBundle />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesRateLock" ? (
-      //       <IconFeaturesRateLock />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesOnlineServicing" ? (
-      //       <IconFeaturesOnlineServicing />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesNonOwner" ? (
-      //       <IconFeaturesNonOwner />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesNoCreditCheck" ? (
-      //       <IconFeaturesNoCreditCheck />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesLowDownPayment" ? (
-      //       <IconFeaturesLowDownPayment />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesDriverExclusion" ? (
-      //       <IconFeaturesDriverExclusion />
-      //     ) : featureNamesArray[i].icon === "IconFeaturesCreditCard" ? (
-      //       <IconFeaturesCreditCard />
-      //     ) : featureNamesArray[i].icon === "IconFeatures247Support" ? (
-      //       <IconFeatures247Support />
-      //     ) : null;
-
-      //   featuresArray.push(pushable);
-
-      if (iconName === "IconFeaturesBuyOnline") {
-        featuresArray.push(<IconFeaturesBuyOnline key={i} />);
-      }
-      if (featureNamesArray[i].icon === "IconFeaturesLocalAgent") {
-        featuresArray.push(<IconFeaturesLocalAgent key={i} />);
-      }
-      if (featureNamesArray[i].icon === "IconFeaturesMobile") {
-        featuresArray.push(<IconFeaturesMobile key={i} />);
-      }
-      if (featureNamesArray[i].icon === "IconFeaturesPaperless") {
-        featuresArray.push(<IconFeaturesPaperless key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesNewCarReplacement") {
-        featuresArray.push(<IconFeaturesNewCarReplacement key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesImmediateCoverage") {
-        featuresArray.push(<IconFeaturesImmediateCoverage key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesAutomaticPayment") {
-        featuresArray.push(<IconFeaturesAutomaticPayment key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesOnlineIdCards") {
-        featuresArray.push(<IconFeaturesOnlineIdCards key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesSR22") {
-        featuresArray.push(<IconFeaturesSR22 key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesRoadside") {
-        featuresArray.push(<IconFeaturesRoadside key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesRideshare") {
-        featuresArray.push(<IconFeaturesRideshare key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesRentersBundle") {
-        featuresArray.push(<IconFeaturesRentersBundle key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesRateLock") {
-        featuresArray.push(<IconFeaturesRateLock key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesOnlineServicing") {
-        featuresArray.push(<IconFeaturesOnlineServicing key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesNonOwner") {
-        featuresArray.push(<IconFeaturesNonOwner key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesNoCreditCheck") {
-        featuresArray.push(<IconFeaturesNoCreditCheck key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesLowDownPayment") {
-        featuresArray.push(<IconFeaturesLowDownPayment key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesDriverExclusion") {
-        featuresArray.push(<IconFeaturesDriverExclusion key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeaturesCreditCard") {
-        featuresArray.push(<IconFeaturesCreditCard key={i} />);
-      }
-
-      if (featureNamesArray[i].icon === "IconFeatures247Support") {
-        featuresArray.push(<IconFeatures247Support key={i} />);
-      }
+      featuresComponentArray.push(featuresObject[iconName]);
     }
-    return featuresArray;
+    return featuresComponentArray;
   };
 
   return <div> {buildFeatures(props.features)}</div>;
